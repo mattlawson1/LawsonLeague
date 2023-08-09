52cdc34d3cb17f7bea3bba89578d2dab85303242
mattlawson1 <88358065+mattlawson1@users.noreply.github.com>
2023-08-09 17:40:02 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,19 +32,19 @@ export const managers = [
 
      "location": "Allentown", // (optional)
 
-     "bio": "Lorem ipsum...",
+     "bio": "Just a couple guys bein dudes",
 
      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
 
-     "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
+     "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
 
-     "favoriteTeam": "nyj", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+     "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
 
      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
 
      "rival": {
 
-       name: "Rival", // Can be anything (usually your rival's name)
+       name: "Matt", // Can be anything (usually your rival's name)
 
        link: 6, // manager array number within this array, or null to link back to all managers page
 
@@ -52,15 +52,15 @@ export const managers = [
 
      },
 
-     "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+     "favoritePlayer": 6904, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
 
      "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
 
      "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
 
-     "philosophy": "Your fantasy team's philosophy", // (optional)
+     "philosophy": "Score points or go in the dungeon", // (optional)
 
-     "tradingScale": 10, // 1 - 10 (optional)
+     "tradingScale": 7, // 1 - 10 (optional)
 
      "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
 
