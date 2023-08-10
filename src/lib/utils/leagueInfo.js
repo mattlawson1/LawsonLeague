c3420e4ee0098b470d2eb1767a67357e24c0cd57
mattlawson1 <88358065+mattlawson1@users.noreply.github.com>
2023-08-10 01:39:38 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -532,7 +532,7 @@ export const managers = [
 
       "managerID": "739251210330230784",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
 
-      "name": "Dad Lawson",
+      "name": "Mel Lawson",
 
       "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
 
