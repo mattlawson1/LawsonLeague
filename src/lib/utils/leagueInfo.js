131bbe201758a95e18c5cab2ec7ccbc80ab64911
mattlawson1 <88358065+mattlawson1@users.noreply.github.com>
2024-08-01 14:31:32 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -348,27 +348,27 @@ export const managers = [
 
       "managerID": "870091388984025088",  // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
 
-      "name": "Skylar Vilaychithc",
+      "name": "Kevin Bolick",
 
       "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
 
-      "location": "Harrisburg", // (optional)
+      "location": "Georgia", // (optional)
 
-      "bio": "A bunch of hungry dawgs that are gonna play in the trenches",
+      "bio": "I am here.",
 
       "photo": "/managers/skylar.jpg", // square ratio recommended (no larger than 500x500)
 
-      "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
 
-      "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
 
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
 
       "rival": {
 
-        name: "Julia", // Can be anything (usually your rival's name)
+        name: "Meredith", // Can be anything (usually your rival's name)
 
-        link: 9, // manager array number within this array, or null to link back to all managers page
+        link: 2, // manager array number within this array, or null to link back to all managers page
 
         image: "/managers/julia.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
 
@@ -380,9 +380,9 @@ export const managers = [
 
       "rookieOrVets": "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
 
-      "philosophy": "All gas no brakes.", // (optional)
+      "philosophy": "L", // (optional)
 
-      "tradingScale": 4, // 1 - 10 (optional)
+      "tradingScale": 10, // 1 - 10 (optional)
 
       "preferredContact": "Text",  // (optional) 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
 
